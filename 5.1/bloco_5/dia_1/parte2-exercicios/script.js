5f907b2917b857a82bf6ae62ed73b3b613ea4ffa
Thiago <thiagodesouza.dev@gmail.com>
2021-03-04 09:43:55 -0300

    bloco 5

---
+++ 5.1/bloco_5/dia_1/parte2-exercicios/script.js
@@ -0,0 +1,6 @@
+var paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
+
+document.getElementById("page-title").innerText = "senhor dos aneis";
+document.getElementById("second-paragraph").innerText = "gandalf morreu #triste";
+document.getElementById("subtitle").innerText = "morte de gandalf";
