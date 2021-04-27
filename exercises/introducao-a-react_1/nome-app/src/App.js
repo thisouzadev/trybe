db79a9c083c9273a21323cb9ccfd55efbf8bb267
Thiago <thiagodesouza.dev@gmail.com>
2021-04-27 08:48:48 -0300

    iniciando em react

---
+++ exercises/introducao-a-react_1/nome-app/src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './App.css';
+
+const tarefas = ["Acordar", "Tomar café", "Escovar os dentes", "Ir trabalhar"];
+
+const App = () => ( <ul>{tarefas.map(tarefa => <li>{ tarefa }</li>)}</ul> );
+
+export default App;
