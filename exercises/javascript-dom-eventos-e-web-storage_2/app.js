732be8e35e69f36f2a002534accd43930deb7fed
Thiago <thiagodesouza.dev@gmail.com>
2021-05-27 19:22:16 -0300

    add h1

---
+++ exercises/javascript-dom-eventos-e-web-storage_2/app.js
@@ -0,0 +1,6 @@
+const elemento_pai = document.body;
+let titulo = document.createElement('h1');
+elemento_pai.appendChild(titulo);
+let texto =  document.createTextNode('Exercício 5.2 - JavaScript DOM');
+titulo.appendChild(texto);
+
