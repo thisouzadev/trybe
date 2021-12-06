096d738ba9490fad3d51c3ded2eea7a19bbe0fd8
Thiago Souza <78744052+thisouzadev@users.noreply.github.com>
2021-12-06 12:46:01 -0300

    finalizado

---
+++ back-end/bloco-24-mongodb-updates-simples-e-complexos/dia-1-updates-simples/dataset.js
@@ -0,0 +1,17 @@
+db.students.insertMany(
+  [
+    { name: "Leppa ping", school: "Escola dos Pigs", bestScore: 9.8 },
+    { name: "Parry Hotter", school: "Googwarts", bestScore: 7.2 },
+    { name: "Gilherme I", school: "Turma 13", bestScore: 9 },
+    { name: "Gilherme II", school: "Turma 13", bestScore: 6 },
+    { name: "Gilherme III", school: "Turma 13", bestScore: 8.2 },
+    { name: "Gilherme IV", school: "Turma 13", bestScore: 2.2 },
+    { name: "Gilherme V", school: "Turma 13", bestScore: 10 },
+    { name: "Gilherme VI", school: "Turma 13", bestScore: 6.7 },
+    { name: "Gilherme VII", school: "Turma 13", bestScore: 9.5 },
+    { name: "Gilherme VIII", school: "Turma 13", bestScore: 8.8 },
+    { name: "Gilherme IX", school: "Turma 13", bestScore: 3 },
+    { name: "Gilherme X", school: "Turma 13", bestScore: 0.5 },
+    { name: "Batiman", school: "Botam", bestScore: 12 }
+  ]
+);
