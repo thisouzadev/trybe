50ad7289a8a0a0bd4b113911a358db488237dfb9
Thiago Souza <78744052+thisouzadev@users.noreply.github.com>
2022-03-29 16:06:08 -0300

    finish

---
+++ ciencia-da-computacao/bloco-33-introducao-a-python/dia-1-aprendendo-python/atividade/bonus/exercicio4.py
@@ -0,0 +1,20 @@
+# Exercício 4: Um posto está vendendo combustíveis com a
+# seguinte tabela de descontos:
+
+def fuel_price(type, liters):
+    if type == "A":
+        price = 1.90
+        discount = 0.03
+        if liters > 20:
+            discount = 0.05
+    elif type == "G":
+        price = 2.50
+        discount = 0.04
+        if liters > 20:
+            discount = 0.06
+    total = price * liters
+    total -= total * discount
+    return total
+
+
+print(fuel_price('A', 5))
