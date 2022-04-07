83d8a46c9e0d25f5c7fea8b3a240aad881ba2485
Thiago Souza <78744052+thisouzadev@users.noreply.github.com>
2022-04-07 13:42:08 -0300

    initial commit

---
+++ ciencia-da-computacao/bloco-34-programacao-orientada-a-objetos-e-padroes-de-projeto/dia-1-introducao-a-programacao-orientada-a-objetos/conteudo/quadrado.py
@@ -0,0 +1,26 @@
+# Nome da abstração
+# Quadrado
+
+# atributos/estados
+# - lado (tamanho)
+
+# comportamentos
+# - calcular area (lado * lado)
+# - calcular perímetro (4 * lado)
+
+class Retangulo:
+    def __init__(self, base, altura):
+        self.base = base
+        self.altura = altura
+
+    def calcular_area(self):
+        return self.base * self.altura
+
+    def calcular_perimetro(self):
+        return self.base * 2 + self.altura * 2
+
+
+# Para testar!
+retangulo_1 = Retangulo(5)
+print(retangulo_1.calcular_area())
+print(retangulo_1.calcular_perimetro())
